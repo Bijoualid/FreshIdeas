bc0a080d22100571aec8914a01476b80df2cf3f0
Bijoualid <47030034+Bijoualid@users.noreply.github.com>
2019-06-28 12:02:12 +0200

    New website

---
+++ script.js
@@ -0,0 +1,11 @@
+/*var ps = document.querySelectorAll("p");
+
+for (var i = 0; i < ps.length; i++) {
+
+	(function (p) {
+		window.setInterval(function () {
+			p.classList.toggle("pink")
+		}, 1500)
+	}(ps[i]))
+};
+*/
